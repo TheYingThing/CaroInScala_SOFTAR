55c97afcbd2ee664a37faa737820b2d615f882d2
rebeccabraun <you@example.com>
2020-11-20 11:39:01 +0100

    added tests for Rules and made changes to Rules

--- src/main/scala/caro/model/Rules.scala
+++ src/main/scala/caro/model/Rules.scala
@@ -12,112 +12,107 @@ class Rules(board:Board){
   def getLowerLeft(row:Int, col:Int):Cell = board.getCell(row + 1, col - 1)
   def getLowerRight(row:Int, col:Int):Cell = board.getCell(row + 1, col + 1)
 
-  def allRules(row:Int, col:Int, cell:Cell):Boolean = {
-    return sameColor(col, row, cell) && onEdge(row, col) && diagonal(row, col, cell) && maxColor(row, col , cell)
-      && maxField(row, col)
-
+  def allRules(row:Int, col:Int, color:String):Boolean = {
+    if(board.isEmpty)
+      true
+    else
+      sameColor(row, col, color)&&onEdge(row, col)&&diagonal(row, col, color)&&maxColor(row, col, color)&&maxField(row, col)
   }
 
-  def sameColor(row:Int, col:Int, cell:Cell):Boolean = {
-    if (getRight(row, col).isOccupied) {
-      if (getRight(row, col).getColor.equals(cell.getColor)) {
+  //returns true when tile can be laid
+  def sameColor(row:Int, col:Int, color:String):Boolean = {
+
+      if (getRight(row, col).getColor.equals(color)) {
+         return false
+      }
+      if(getLeft(row, col).getColor.equals(color)) {
+          return false
+      }
+      if(getOver(row, col).getColor.equals(color)) {
+         return false
+      }
+      if(getUnder(row,col).getColor.equals(color)) {
         return false
       }
-    } else if(getLeft(row,col).isOccupied) {
-      if(getLeft(row, col).getColor.equals(cell.getColor)) {
-        return false
-      }
-    } else if(getOver(row,col).isOccupied) {
-      if(getOver(row, col).getColor.equals(cell.getColor)) {
-        return false
-      }
-    } else if(getUnder(row,col).isOccupied) {
-      if(getUnder(row,col).getColor.equals(cell.getColor)) {
-        return false
-      }
-    }
+
      true
   }
-//return true when placed next to already placed tile
+
+  //returns true when tile can be laid
   def onEdge(row:Int, col:Int):Boolean = {
-     if (getRight(row, col).isOccupied) {
-      return true
-    } else if(getLeft(row, col).isOccupied) {
-      return true
-     }else if (getOver(row, col).isOccupied) {
-      return true
-    } else if(getUnder(row, col).isOccupied) {
-      return true
-    }
-    false
+
+     if (getRight(row, col).isOccupied)
+        true
+     else if(getLeft(row, col).isOccupied)
+        true
+     else if (getOver(row, col).isOccupied)
+        true
+     else if(getUnder(row, col).isOccupied)
+        true
+     else
+       false
+
+
+
   }
-//return true when diagonal is not full
-  def diagonal(row:Int, col:Int, cell:Cell):Boolean ={
-    var counterDiag1:Int = 0
-    var counterDiag2:Int = 0
 
-    while (counterDiag1 < 3 && counterDiag2 < 3) {
+  //returns true when tile can be laid
+  def diagonal(row:Int, col:Int, color:String):Boolean ={
 
-      if(getUpperLeft(row, col).getColor.equals(cell.getColor)) {
-        counterDiag1 = counterDiag1 + 1
-        if(getUpperLeft(row -1, col-1).getColor.equals(cell.getColor)) {
-          counterDiag1 = counterDiag1- + 1
-          if(getUpperLeft(row -2, col-2 ).getColor.equals(cell.getColor)) {
-            counterDiag1 = counterDiag1 + 1
-          }
-        }
-      }
+    var counter1:Int = 0
+    var counter2:Int = 0
 
-      if(getLowerRight(row, col).getColor.equals(cell.getColor)) {
-        counterDiag1 = counterDiag1 + 1
-        if(getLowerRight(row + 1, col + 1).getColor.equals(cell.getColor)) {
-          counterDiag1 = counterDiag1 + 1
-          if(getLowerRight(row + 2, col + 2).getColor.equals(cell.getColor)) {
-            counterDiag1 = counterDiag1 + 1
-          }
-        }
-      }
+    if(getUpperLeft(row, col).getColor.equals(color))
+      counter1 += 1
+      if(getUpperLeft(row - 1, col - 1).getColor.equals(color))
+        counter1 += 1
+        if(getUpperLeft(row - 2, col - 2).getColor.equals(color))
+          counter1 += 1
 
-      if(getUpperRight(row, col).getColor.equals(cell.getColor)) {
-        counterDiag2 = counterDiag2 + 1
-        if(getUpperRight(row -1, col + 1).getColor.equals(cell.getColor)) {
-          counterDiag2 = counterDiag2- + 1
-          if(getUpperRight(row -2, col + 2 ).getColor.equals(cell.getColor)) {
-            counterDiag2 = counterDiag2 + 1
-          }
-        }
-      }
-      if(getLowerLeft(row, col).getColor.equals(cell.getColor)) {
-        counterDiag2 = counterDiag2 + 1
-        if(getLowerLeft(row + 1, col - 1).getColor.equals(cell.getColor)) {
-          counterDiag2 = counterDiag2 + 1
-          if(getLowerLeft(row + 2, col - 2).getColor.equals(cell.getColor)) {
-            counterDiag2 = counterDiag2 + 1
-          }
-        }
-      }
-       return true
-    }
-    false
+    if(getLowerRight(row, col).getColor.equals(color))
+      counter1 += 1
+      if(getLowerRight(row + 1, col + 1).getColor.equals(color))
+        counter1 += 1
+        if(getLowerRight(row + 2, col + 2).getColor.equals(color))
+          counter1 += 1
+
+    if(getUpperRight(row, col).getColor.equals(color))
+      counter2 += 1
+      if(getUpperRight(row -1, col + 1).getColor.equals(color))
+        counter2 += 1
+        if(getUpperRight(row -2, col + 2 ).getColor.equals(color))
+          counter2 += 1
+
+    if(getLowerLeft(row, col).getColor.equals(color))
+      counter2 += 1
+      if(getLowerLeft(row + 1, col - 1).getColor.equals(color))
+        counter2 += 1
+        if(getLowerLeft(row + 2, col - 2).getColor.equals(color))
+          counter2 += 2
+
+    counter1 <=2 && counter2 <= 2
   }
-//returns true when tile can be laid
-  def maxColor(row:Int, col:Int, cell:Cell):Boolean = {
-    twoColor(row-1, col, cell)&&twoColor(row+1, col, cell)&&twoColor(row, col+1, cell)&&twoColor(row, col-1, cell)
+
+  //return true when tile can be laid
+  def maxColor(row:Int, col:Int, color:String):Boolean = {
+    twoColor(row-1, col, color)&&twoColor(row+1, col, color)&&twoColor(row, col+1, color)&&twoColor(row, col-1, color)
   }
-//return true when less than tow of same color
-  def twoColor(row:Int, col:Int, cell:Cell):Boolean = {
+
+  //return true when less than two of same color
+  def twoColor(row:Int, col:Int, color:String):Boolean = {
     var counter:Int = 0
-    if (getOver(row, col).getColor == cell.getColor)
+    if (getOver(row, col).getColor == color)
       counter += 1
-    if (getUnder(row, col).getColor == cell.getColor)
+    if (getUnder(row, col).getColor == color)
       counter += 1
-    if (getLeft(row, col).getColor == cell.getColor)
+    if (getLeft(row, col).getColor == color)
       counter += 1
-    if (getRight(row, col).getColor == cell.getColor)
+    if (getRight(row, col).getColor == color)
       counter += 1
     counter < 2
   }
-//return true when able to place new tile
+
+  //return true when tile can be laid
   def maxField(row:Int, col:Int):Boolean = {
     if(board.getHeight == maxSize && board.rowEmpty(row))
       return false
@@ -125,7 +120,5 @@ class Rules(board:Board){
       return false
     true
   }
-
-
 
 }
