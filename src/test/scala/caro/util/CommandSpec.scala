9aab1c00ed8f9a4140c42b8a704b66893745fbed
Ying-Ling Dang <yingling.dang@gmail.com>
2020-12-10 18:44:16 +0100

    Added UndoManagerSpec.scala and CommandSpec.scala

---
+++ src/test/scala/caro/util/CommandSpec.scala
@@ -0,0 +1,46 @@
+package caro.util
+
+import org.scalatest.matchers._
+import org.scalatest.wordspec._
+
+class incrCommand extends Command {
+  var state:Int =0
+  override def doStep(): Unit = state+=1
+
+  override def undoStep(): Unit = state-=1
+
+  override def redoStep(): Unit = state+=1
+}
+
+class CommandSpec extends AnyWordSpec with should.Matchers {
+  "A Command" should {
+
+    "have a do step" in {
+      val command = new incrCommand
+      command.state should be(0)
+      command.doStep()
+      command.state should be(1)
+      command.doStep()
+      command.state should be(2)
+
+    }
+    "have an undo step" in {
+      val command = new incrCommand
+      command.state should be(0)
+      command.doStep()
+      command.state should be(1)
+      command.undoStep()
+      command.state should be(0)
+    }
+    "have a redo step" in {
+      val command = new incrCommand
+      command.state should be(0)
+      command.doStep()
+      command.state should be(1)
+      command.undoStep()
+      command.state should be(0)
+      command.redoStep()
+      command.state should be(1)
+    }
+  }
+}
