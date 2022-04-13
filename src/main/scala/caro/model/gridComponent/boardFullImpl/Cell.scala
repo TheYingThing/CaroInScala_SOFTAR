fc5bbf8e50d8cb1d201ee9291f760be3bd0980e0
rebeccaBraun <rebaccabraun6@gmail.com>
2022-04-13 11:01:21 +0200

    made color to enum

--- src/main/scala/caro/model/gridComponent/boardFullImpl/Cell.scala
+++ src/main/scala/caro/model/gridComponent/boardFullImpl/Cell.scala
@@ -2,13 +2,21 @@ package caro.model.gridComponent.boardFullImpl
 
 import caro.model.gridComponent.CellInterface
 
-case class Cell(color: Option[String]) extends CellInterface :
+case class Cell(color: Option[Color]) extends CellInterface :
   def isOccupied: Boolean = color.isDefined
 
   def getColor: String = {
     color match {
-      case Some(t) => t
+      case Some(t) => t.toString
       case None => "none"
     }
   }
 end Cell
+
+enum Color:
+  case RED
+  case BLACK
+  case GREY
+  case WHITE
+end Color
+
